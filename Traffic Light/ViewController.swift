9c0fd8593329193694249d9316f3c3afb281c16d
Oleg <oleg.zuev.2012@gmail.com>
2025-04-05 15:37:37 +0300

    work with UI

--- Traffic Light/ViewController.swift
+++ Traffic Light/ViewController.swift
@@ -14,28 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet var greenView: UIView!
     @IBOutlet var trafficLightButton: UIButton!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        redView.layer.cornerRadius = 80
+        redView.layer.cornerRadius = 60
         redView.alpha = 0.3
-        yellowView.layer.cornerRadius = 80
+        yellowView.layer.cornerRadius = 60
         yellowView.alpha = 0.3
-        greenView.layer.cornerRadius = 80
+        greenView.layer.cornerRadius = 60
         greenView.alpha = 0.3
-        
+        trafficLightButton.layer.cornerRadius = 5
+    
     }
     
     private var counter = 0
     private enum TurnOnColour: Int {
-        case green = 2
-        case yellow = 1
         case red = 0
+        case yellow = 1
+        case green = 2
     }
    
     @IBAction func trafficLightButtonDidTapped(_ sender: UIButton) {
         sender.setTitle("Next", for: .normal)
-        
         
             switch TurnOnColour(rawValue: counter) {
             case .red:
@@ -57,11 +56,7 @@ class ViewController: UIViewController {
             }
         
         
-        if counter < 2 {
-            counter += 1
-        } else {
-            counter = 0
-        }
+         counter = counter < 2 ? counter + 1 : 0
         
     }
     
